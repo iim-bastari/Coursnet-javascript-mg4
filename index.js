d8351aa8bfa5b65f6cfff05561f8dfa2c203d671
iim-bastari <iimbastian320@gmail.com>
2022-09-25 14:57:13 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// Cara mendeklarasikan Variabel
+
+// const Number1 = prompt('Number1')
+// const Number2 = prompt('Number2')
+// const Result = Number1 + Number2
+// alert('Hasil :' + Result)
+// // alert(`Hasil Bagi : ${Result}`)
+
+
+
+
+// const usiaAyah = 5
+// const usiaIbu = 10
+// console.log('USIA', usiaAyah * usiaIbu)
